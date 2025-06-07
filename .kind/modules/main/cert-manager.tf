18d0bb41f5b0685f275d8f2a101c569f896c1128
Ryan Faircloth <ryan@dss-i.com>
2025-06-07 11:12:11 -0500

    feat: add trust manager

--- .kind/modules/main/cert-manager.tf
+++ .kind/modules/main/cert-manager.tf
@@ -19,11 +19,30 @@ resource "kubectl_manifest" "cert_manager" {
     }
   }
 }
+
+# Deploy trust-manager HelmRelease using the jetstack chart
+resource "kubectl_manifest" "trust_manager_helmrelease" {
+  yaml_body          = file("${path.module}/flux2-manifests/trust-manager-helmrelease.yaml")
+  override_namespace = kubernetes_namespace.cert_manager.metadata[0].name
+  depends_on         = [kubectl_manifest.jetstack_helmrepository, kubectl_manifest.cert_manager]
+  wait_for {
+    field {
+      key   = "status.conditions.[0].status"
+      value = "True"
+    }
+  }
+}
+
+
 # Deploy cluster-issuer HelmRelease using the bedag/raw chart
 resource "kubectl_manifest" "cluster_issuer_helmrelease" {
   yaml_body          = file("${path.module}/flux2-manifests/cluster-issuer-helmrelease.yaml")
   override_namespace = kubernetes_namespace.cert_manager.metadata[0].name
-  depends_on         = [kubectl_manifest.bedag_helmrepository, kubectl_manifest.cert_manager]
+  depends_on = [
+    kubectl_manifest.bedag_helmrepository,
+    kubectl_manifest.cert_manager,
+    kubectl_manifest.trust_manager_helmrelease
+  ]
   wait_for {
     field {
       key   = "status.conditions.[0].status"
